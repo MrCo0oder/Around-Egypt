08d0695b9d4af52e9acd45fef935daa88d49fd72
Ahmed Ehab <ahmedbadawiehab@gmail.com>
2025-03-16 03:40:56 +0200

    perf: Disable Minification and Source File Renaming
    This commit disables code minification for release builds and turns off source file renaming in ProGuard.
    
    - **Minification:**
      - Disabled `isMinifyEnabled` in the `release` build type within `build.gradle.kts`.
    - **Source File Renaming:**
      - Commented out `-renamesourcefileattribute SourceFile` in `proguard-rules.pro` to prevent source file renaming during obfuscation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
             isDebuggable = true
         }
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
